7620f7d9824a912d0a9d670b500b3d5fedfe8aec
Manuel Liriano <manuelliriano.ml@gmail.com>
2019-10-24 17:37:59 -0400

    Second

--- app/src/main/java/com/learning/manuelliriano/registro/MainActivity.java
+++ app/src/main/java/com/learning/manuelliriano/registro/MainActivity.java
@@ -2,12 +2,22 @@ package com.learning.manuelliriano.registro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner mySpinner;
+    private String[] data = {"Soltero/a", "Casado/a", "Union Libre", "Viudo", "Esperando un milagro"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mySpinner= findViewById(R.id.mySpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, data);
+        mySpinner.setAdapter(adapter);
+
     }
 }
